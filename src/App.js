0f4a5b265c52f454255ca3de0dc91966f366d9d2
Cameron <cameronkwalden@gmail.com>
2022-11-25 13:31:36 -0800

    moves definition to own component, places in Card

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 import { FormControl, InputLabel, Input, Button } from "@mui/material";
+import Definition from "./components/Definition";
 
 function App() {
   const [word, setWord] = useState("");
   const [definition, setDefinition] = useState([]);
-  // https://api.dictionaryapi.dev/api/v2/entries/en/<word>
-
-  // https://api.dictionaryapi.dev/api/v2/entries/en/hello
 
   const getDefinition = async () => {
     try {
       let getWord = await axios.get(
         `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
       );
-      console.log(getWord.data, "getWord.data");
+      // console.log(getWord.data, "getWord.data");
       setDefinition(getWord.data);
     } catch (error) {
       console.log(error);
@@ -31,13 +29,7 @@ function App() {
         <Input id="my-input" aria-describedby="my-helper-text" />
         <Button onClick={getDefinition}>SEARCH</Button>
       </FormControl>
-      {word
-        ? definition.map((meaning) =>
-            meaning.meanings.map((def) =>
-              def.definitions.map((defDef, idx) => <p key={idx}>{defDef.definition}</p>)
-            )
-          )
-        : null}
+      <Definition word={word} definition={definition} />
     </div>
   );
 }
