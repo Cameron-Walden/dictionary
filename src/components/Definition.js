52b90ac1ccaf9da81b7e4f193c24b44850a21f9b
Cameron <cameronkwalden@gmail.com>
2022-11-28 13:14:23 -0800

    fixes issue with card rendering before submission

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -2,10 +2,10 @@ import { Card, CardContent, Grid } from "@mui/material";
 import Phonetics from "./Phonetics";
 import DefinitionAccordion from "./DefinitionAccordion";
 
-export default function Definition({ word, definition }) {
+export default function Definition({ definition, hasSubmitted }) {
   return (
     <Grid container direction="column" alignItems="center" justify="center">
-      {word ? (
+      {hasSubmitted ? (
         <Card sx={{ minWidth: 350, maxWidth: 350 }}>
           <CardContent>
             <Phonetics definition={definition} />
