a29a984b24039e7e242e76398d19dcb3abd44471
Cameron <cameronkwalden@gmail.com>
2022-11-30 17:54:43 -0800

    adds transition to popper

--- src/components/Synonym.js
+++ src/components/Synonym.js
@@ -1,27 +1,37 @@
-import { Button, Box, Popper } from "@mui/material";
+import { Button, Box, Popper, Fade } from "@mui/material";
 import { useState } from "react";
 
 export default function Synonym({ definition }) {
   const [newPopper, setNewPopper] = useState(null);
+  const [openPopper, setOpenPopper] = useState(false);
 
-  const handlePopper = (e) => setNewPopper(newPopper ? null : e.currentTarget);
+  const handlePopper = (e) => {
+    setNewPopper(newPopper ? null : e.currentTarget);
+    setOpenPopper((prevOpen) => !prevOpen);
+  };
 
-  const open = Boolean(newPopper);
-  const id = open ? "simple-popper" : undefined;
+  const isOpen = openPopper && Boolean(newPopper);
+  const id = isOpen ? "transition-popper" : undefined;
 
   return (
     <div>
       <Button variant="outlined" aria-describedby={id} onClick={handlePopper}>
         Synonyms
       </Button>
-      <Popper id={id} open={open} anchorEl={newPopper}>
-        <Box sx={{ border: 2, p: 1, bgcolor: "background.paper" }}>
-          {definition.map((def) =>
-            def.meanings.map((mean) =>
-              mean.synonyms.map((synonym, idx) => <p key={idx}>{synonym}</p>)
-            )
-          )}
-        </Box>
+      <Popper id={id} open={openPopper} anchorEl={newPopper} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={750}>
+            <Box sx={{ border: 2, p: 1, bgcolor: "background.paper" }}>
+              {definition.map((def) =>
+                def.meanings.map((mean) =>
+                  mean.synonyms.map((synonym, idx) => (
+                    <p key={idx}>{synonym}</p>
+                  ))
+                )
+              )}
+            </Box>
+          </Fade>
+        )}
       </Popper>
     </div>
   );
